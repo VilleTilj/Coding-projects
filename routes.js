5d5c08094d011a2844d3ab0217a2799ed4740fe2
Tiljander Ville <ville.tiljander@tuni.fi>
2020-11-18 00:25:27 +0200

    Add to routes new user model. Hope it does not break everything. Fix if needed

--- routes.js
+++ routes.js
@@ -1,12 +1,11 @@
 const responseUtils = require('./utils/responseUtils');
 const { acceptsJson, isJson, parseBodyJson } = require('./utils/requestUtils');
 const { renderPublic } = require('./utils/render');
-const { emailInUse, getAllUsers, saveNewUser, validateUser, updateUserRole, getUserById } = require('./utils/users');
-const { deleteUserById, requestedUsername} = require('./utils/users');
+const getUser = require('./models/user');
 const { basicAuthChallenge, notFound, sendJson, badRequest, unauthorized} = require('./utils/responseUtils');
 const { getCurrentUser } = require('./auth/auth');
 const productdata = require('./products.json').map(function(product){({ ...product })});
-const users = getAllUsers();
+//const users = getAllUsers();
 /**
  * Known API routes and their allowed methods
  *
@@ -82,8 +81,8 @@ const handleRequest = async (request, response) => {
       if (user){
         if( user.role === 'admin'){
           const reqName = filePath.split('/').pop();
-          const reqUser = getUserById(reqName);
-          if (reqUser in users){  
+          const reqUser = await getUser.findOne({name: reqName}).exec();
+          if (reqUser){  
             if ( method.toUpperCase() === 'GET') { 
               return responseUtils.sendJson(response, reqUser);
             }
@@ -92,8 +91,10 @@ const handleRequest = async (request, response) => {
               // if role can be found
               if (updateRequest.role) {
                 try {            
-                  const updatedUser = updateUserRole(reqUser.name, updateRequest.role);
-                  return responseUtils.sendJson(response, updatedUser);
+                  reqUser.role = updateRequest.role;
+                  await reqUser.save();
+                  //const updatedUser = updateUserRole(reqUser.name, updateRequest.role);
+                  return responseUtils.sendJson(response, reqUser);
                 }
                 catch (err) { badRequest(response, err);}
               } else { 
@@ -102,7 +103,11 @@ const handleRequest = async (request, response) => {
             }
             if (method.toUpperCase() === 'DELETE') {
               // try to delete user
-              const deleted = deleteUserById(reqUser.name);
+              await getUser.deleteOne({_id: reqUser._id}, function(err) {
+                if(err) console.log(err);
+                console.log("Delete succesfull");
+              });
+              //const deleted = deleteUserById(reqUser.name);
               if (deleted) {
                 return responseUtils.sendJson(response, deleted);
               }
@@ -115,7 +120,7 @@ const handleRequest = async (request, response) => {
       } else {
         return basicAuthChallenge(response);
       }
-     }else {
+     } else {
       unauthorized(response);
     }
   }
@@ -178,19 +183,38 @@ const handleRequest = async (request, response) => {
     // TODO: 8.3 Implement registration
     // You can use parseBodyJson(request) from utils/requestUtils.js to parse request body
     // throw new Error('Not Implemented');
+
+    // This seems quite off.. Must be better way.
     const registerUser = await (parseBodyJson(request));
-    const problems = validateUser(registerUser);
+    const emailUser = await getUser.findOne({ email: registerUser.email }).exec();
+    const nameUser = await getUser.findOne({ name: registerUser.name }).exec();
+    const passwordUser = await getUser.findOne({ password: registerUser.password }).exec();
+    const roleUser = await getUser.findOne({ role: registerUser.role }).exec();
+
+    var problems = [];
+    if (emailUser !== true) {	problems.push("Missing email");}
+    if (nameUser !== true) {	problems.push("Missing name");}
+    if (passwordUser !== true) {	problems.push("Missing password");}
+    if (roleUser !== true) {	problems.push("Missing role");}
+    
 
     // Check if there are errors in creating user and that the email is not in use allready
-    if ( problems.length !== 0 || emailInUse(registerUser.email)) {
+    
+    if ( problems.length !== 0 || emailUser) {
       return responseUtils.badRequest(response, "400 Bad Request");
     } else {     
       // Set new user to the resourses
-      let newUser = saveNewUser(registerUser);
-      newUser = updateUserRole(newUser._id, 'customer');
+      
+      //let newUser = saveNewUser(registerUser);
+      let newUser = registerUser;
+      //newUser = updateUserRole(newUser._id, 'customer');
+      newUser.role = "customer";
+
       return responseUtils.createdResource(response, newUser);
     }
   }
 };
+
+
 
 module.exports = { handleRequest };
