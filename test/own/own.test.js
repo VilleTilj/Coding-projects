ffcab715f4b0cd0d6cc77cff8f982cabd810616f
Miika Kuusisto <miika.kuusisto@tuni.fi>
2020-11-19 14:30:19 +0200

    testing 10.3 'mocha testing'

---
+++ test/own/own.test.js
@@ -0,0 +1,56 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Tests', () => {
+  
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(1);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+2).to.equal(2);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(3);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(4);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(5);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(6);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(7);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(8);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(9);
+    });
+  });
+  describe('Testing first test', () => {
+    it('tests 1+1, should return 2"', () => {
+		expect(1+1).to.equal(10);
+    });
+  });  
+});
